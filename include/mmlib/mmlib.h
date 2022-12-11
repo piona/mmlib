cadf2dc074506fed4f81d1d1c8c7751ec25d22fe
Piotr Nazimek <pnazimek@gmail.com>
2022-12-12 00:50:41 +0100

    MMLib with printf add/sub

--- include/mmlib/mmlib.h
+++ include/mmlib/mmlib.h
@@ -1,7 +1,7 @@
 #ifndef MMLIB_H_
 #define MMLIB_H_
 
-#define MMLIB_VERSION 2
+#define MMLIB_VERSION 3
 
 #include <mmlib/addsub.h>
 #include <mmlib/muldiv.h>
--- src/addsub.c
+++ src/addsub.c
@@ -1,12 +1,15 @@
 #include <mmlib/addsub.h>
+#include <stdio.h>
 
 int mmlib_add(int x, int y)
 {
+    printf("add: x = %d y = %d\n", x, y);
     return x + y;
 }
 
 int mmlib_sub(int x, int y)
 {
+    printf("sub: x = %d y = %d\n", x, y);
     return x - y;
 }
 
