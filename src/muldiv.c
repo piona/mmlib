50666059bf6038cf135b91d38086bbb8a0069b9a
Piotr Nazimek <pnazimek@gmail.com>
2022-12-12 00:54:01 +0100

    MMLib with printf mul/div

--- src/muldiv.c
+++ src/muldiv.c
@@ -1,12 +1,15 @@
 #include <mmlib/muldiv.h>
+#include <stdio.h>
 
 int mmlib_mul(int x, int y)
 {
+    printf("mul: x = %d y = %d\n", x, y);
     return x * y;
 }
 
 int mmlib_div(int x, int y)
 {
+    printf("div: x = %d y = %d\n", x, y);
     return x / y;
 }
 
